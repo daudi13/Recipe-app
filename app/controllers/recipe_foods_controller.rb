48b7885788a4f2cbd7493a053f8264aae0b5f4a8
daudi13 <davouma13@gmail.com>
2022-06-29 14:12:26 +0300

    Add create method in class reciperFoodController

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -4,5 +4,20 @@ class RecipeFoodsController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
   end
 
-  
+  def create
+    new_recipe_item = RecipeFood.new
+    new_recipe_item.quantity = params[:recipe_food][:quantity]
+    recipe = Recipe.find(params[:recipe_id])
+    food = Food.find(params[:recipe_food][:food])
+    new_recipe_item.recipe_id = recipe.id
+    new_recipe_item.food_id = food.id
+
+    if new_recipe_item.save
+      flash[:notice] = 'You have successfully added a new ingredient'
+      redirect_to recipe_path(recipe)
+    else
+      flash[:notice] = 'Ingredient not added'
+      render :new
+    end
+  end
 end
