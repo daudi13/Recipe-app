2bb69621adb08a04ea71ff19581e533d87abea4e
daudi13 <davouma13@gmail.com>
2022-07-01 07:31:57 +0300

    edit typo

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -2,5 +2,5 @@ class RecipeFood < ApplicationRecord
   belongs_to :food
   belongs_to :recipe
 
-  validates: quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
