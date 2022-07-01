3caa4c185d39ed0faed111d4ecfb32504c997ca6
daudi13 <davouma13@gmail.com>
2022-07-01 13:19:28 +0300

    Add validation for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,8 +3,8 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
 
-  # validates :name, presence: true, length: { maximum: 50 }
-  # validates :description, presence: true
-  # validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  # valudates :preperation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :description, presence: true
+  validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  #validates :preperation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
