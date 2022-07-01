c25bdda3bc97f43900430a90235de942125d5a89
daudi13 <davouma13@gmail.com>
2022-07-01 06:30:52 +0300

    edit typos

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,9 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
+
+  def total
+    price = 0
+    recipe_foods.each { |food_item| price += food_item.quantity * food_item.food.price }
+  end
 end
